8dfbe1832e558a41db1ec939fe1d612ab1c1b523
Malte <96349528+InvisibleMalte@users.noreply.github.com>
2026-05-17 17:54:38 +0200

    Add services section and link stylesheets

--- index.php
+++ index.php
@@ -3,12 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/styles/basic.css">
+    <link rel="stylesheet" href="./assets/styles/index.css">
     <title>Favour Technologies &bull; Startseite</title>
+
 </head>
 <body>
-    <h1 class="name">Favour Technologies</h1>
-    <p class="">Hi, wir sind Favour Technologies. Ein Unternehmen, dass sich persönlich um deine alten, als auch deine neuen Geräte kümmert! </P>
+    <div class="header">
+        <h1 class="name">Favour Technologies</h1>
+        <p class="">Hi, wir sind Favour Technologies. Ein Unternehmen, dass sich persönlich um deine alten, als auch deine neuen Geräte kümmert! </P>
+    </div>
     <br><br>
-    
+    <div class="services">
+        <div class="services-header">
+            <p class="services-header-title">Unsere Dienstleistungen</p>
+        </div>
+        <div class="services-content">
+            <div class="services-content-card">
+                <p class="services-content-card-title">Ankauf deiner alten Geräte</p>
+                <p class="services-content-card-content">Wir kaufen deine alte Technik an, bereiten sie wieder auf und verkaufen sie, um ihnen ein zweites Leben zu ermöglichen.</p>
+                <p class="services-content-card-link">Hier kannst du mehr zu dem Verkauf deiner alten Geräte erfahren.</p>
+            </div>
+            <div class="services-content-card">
+                <p class="services-content-card-title">Verkauf von neuen Geräten</p>
+                <p class="services-content-card-content">Wir verkaufen neue Technik, die frisch auf dem Markt gekommen ist.</p>
+                <p class="services-content-card-link">Hier kannst du mehr zu dem Kauf neuer Geräte erfahren.</p>
+            </div>
+            <div class="services-content-card">
+
+            </div>
+        </div>
+    </div>
 </body>
 </html>
