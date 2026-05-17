e3e3b760650da02315afaa7ab6a9ca3a68ae97b9
Malte <96349528+InvisibleMalte@users.noreply.github.com>
2026-05-17 19:02:09 +0200

    Fix stylesheet path to basics.css

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/styles/basic.css">
+    <link rel="stylesheet" href="./assets/styles/basics.css">
     <link rel="stylesheet" href="./assets/styles/index.css">
     <link rel="icon" href="/assets/images/favicon-32x32.png">
     
